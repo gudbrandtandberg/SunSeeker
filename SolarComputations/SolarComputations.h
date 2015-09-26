701a7a6285e0aa081fdf1354d45a5fb0de167e4a
Gudbrand <gudbrandduff@gmail.com>
2015-09-26 16:59:30 +0700

    Initial Commit

---
+++ SolarComputations/SolarComputations.h
@@ -0,0 +1,19 @@
+//
+//  SolarComputations.h
+//  SolarComputations
+//
+//  Created by Gudbrand Tandberg on 26/09/15.
+//  Copyright (c) 2015 Duff Development. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SolarComputations.
+FOUNDATION_EXPORT double SolarComputationsVersionNumber;
+
+//! Project version string for SolarComputations.
+FOUNDATION_EXPORT const unsigned char SolarComputationsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SolarComputations/PublicHeader.h>
+
+
